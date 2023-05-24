b2d3ff46678f70a6a95e133dcccb5ce5a1c5ade7
HoangTien <46020239+01698215896@users.noreply.github.com>
2023-05-24 13:25:27 +0700

    update2

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ const openai = new OpenAIApi(config);
 app.post("/message", (req, res) => {
     // {prompt : "how are you?"}
   const response = openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: req.body.prompt,
     temperature: 0,
-    max_tokens: 300,
+    max_tokens: 1000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
