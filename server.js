db8f2bb13d13255e6a06f8f1d9f8e2e68b362029
HoangTien <46020239+01698215896@users.noreply.github.com>
2023-06-05 08:25:21 +0700

    first commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
-app.listen(3000,() => {
-    console.log("listening on port 3000");
-})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
 
 app.use(express.json());
 app.use(cors());
@@ -18,17 +18,21 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 app.post("/message", (req, res) => {
-    // {prompt : "how are you?"}
+  // {prompt : "how are you?"}
   const response = openai.createCompletion({
-    model: "text-davinci-003",
+    model: "text-davinci-002",
     prompt: req.body.prompt,
     temperature: 0,
-    max_tokens: 1000,
+    max_tokens: 500,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
   });
-  response.then(data=>{
-    res.send({message: data.data.choices[0].text})
-  }).catch(err=>{res.send({message: err.message});})
+  response
+    .then((data) => {
+      res.send({ message: data.data.choices[0].text });
+    })
+    .catch((err) => {
+      res.send({ message: err.message });
+    });
 });
